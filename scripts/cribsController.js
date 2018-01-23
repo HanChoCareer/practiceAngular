0cbf8bff88d655081ce888426b8d45aeabca0a5b
HanChoCareer <hancho.career@gmail.com>
2018-01-23 16:42:12 -0500

    JSON file won't load

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -1,26 +1,12 @@
 var app = angular.module('ngCribs', ['ui.bootstrap']);
-app.controller('cribsController', function ($scope) {
+app.controller('cribsController', function ($scope, cribsFactory) {
 
-	$scope.cribs = [
-	{
-		"type": "Condo",
-		"price": 220000,
-		"address": "213 Grove Street",
-		"description": "Excellent place, really nice view!"
-	},
-	{
-		"type": "House",
-		"price": 410500,
-		"address": "7823 Winding Way",
-		"description": "Beautiful home with lots of space for a large family"
-	},
-	{
-		"type": "Duplex",
-		"price": 395000,
-		"address": "834 River Lane",
-		"description": "Great neighbourhood and lot's of nice green space."
-	}
-	];
+	$scope.cribs;
 
+	cribsFactory.getCribs().then(function(data) {
+		$scope.Cribs = data.data;
+	}, function(error) {
+		console.log(error)
+	});﻿
 });
 
